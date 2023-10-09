565ed210e9726cd1794690aff54a5b12cdaa6002
ebrahim <myemail@gmail.com>
2023-10-09 15:32:37 +0330

    main and start styling

---
+++ components/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <>
+    <div className="buttons">
+    <button>
+        Show All
+    </button>
+    <button>
+        Show Byid
+    </button>
+    <button>
+        Create
+    </button>
+    <button>
+        Update
+    </button>
+    <button>
+        Delete
+    </button>
+    </div>
+    </>
+  )
+}
